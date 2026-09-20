6702d8399ba9fa170ddb49624dcb0b6579aae855
sokoafrik build farm <sokoafrik-farm01@users.noreply.github.com>
2026-09-20 10:14:29 +0200

    fix: extend header icon hit areas invisibly

--- apps/storefront/src/components/cells/CartDropdown/CartDropdown.tsx
+++ apps/storefront/src/components/cells/CartDropdown/CartDropdown.tsx
@@ -74,7 +74,7 @@ export const CartDropdown = () => {
     >
       <LocalizedClientLink
         href="/cart"
-        className="relative"
+        className="relative before:absolute before:-inset-3 before:content-['']"
         aria-label="Go to cart"
       >
         <CartIcon size={20} />
--- apps/storefront/src/components/cells/UserDropdown/UserDropdown.tsx
+++ apps/storefront/src/components/cells/UserDropdown/UserDropdown.tsx
@@ -31,7 +31,7 @@ export const UserDropdown = ({
     >
       <LocalizedClientLink
         href={isLoggedIn ? "/user" : "/login"}
-        className="relative"
+        className="relative before:absolute before:-inset-3 before:content-['']"
         aria-label="Go to user profile"
       >
         <ProfileIcon size={20} />
